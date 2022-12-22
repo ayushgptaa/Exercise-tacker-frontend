6f82c56cb7b7f56950d65f366c502fae5cf2c53a
ayushgptaa <ayush.gupta.jk@gmail.com>
2022-12-23 01:30:06 +0530

    📦NEW: add Home component

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import Home from './screens/home'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Home />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    margin: 10,
   },
 })
---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import DisplayWorkouts from '../components/DisplayWorkouts'
+
+const Home = () => {
+  return (
+    <View>
+      <DisplayWorkouts />
+    </View>
+  )
+}
+
+export default Home
