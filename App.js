6a337aafb48452f2bc69d31cadb505297d79b306
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-01-01 17:09:45 +0530

    📦NEW: add react-navigation

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+
 import Home from './screens/home'
+import IndWorkout from './screens/IndWorkout'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="IndWorkout" component={IndWorkout} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-    margin: 20,
-  },
-})
