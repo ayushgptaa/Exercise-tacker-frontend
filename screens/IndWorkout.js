15567b27ff525fb25f9d8d351605f7ee56af6528
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-01-09 23:27:09 +0530

    📦NEW: add getExercises in IndWorkout

--- screens/IndWorkout.js
+++ screens/IndWorkout.js
@@ -1,16 +1,35 @@
-import { View, Text } from 'react-native'
+import { useState, useEffect } from 'react'
 
 import AddExercise from '../components/AddExercise'
-import BoldText from '../components/BoldText'
 import Container from '../components/Container'
+import DisplayExercises from '../components/DisplayExercises'
+
+import fetchReq from '../utils/fetchReq'
 
 const IndWorkout = ({ route }) => {
+  const [exercises, setExercises] = useState([])
   const { item } = route.params
-  const { _id, exercises } = item
+  const { _id } = item
+
+  const getExercises = async () => {
+    const config = {
+      method: 'get',
+      url: `http://192.168.0.138:3000/api/workouts/${_id}`,
+    }
+
+    const res = await fetchReq(config)
+
+    if (res.status === 200) setExercises(res.data.exercises)
+  }
+
+  useEffect(() => {
+    getExercises()
+  }, [])
 
   return (
     <Container>
-      <AddExercise id={_id} />
+      <AddExercise id={_id} getExercises={getExercises} />
+      <DisplayExercises exercises={exercises} />
     </Container>
   )
 }
