e70d7ce0e66c4202dbb7aa00b74fed90332a3383
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-02-09 13:15:23 +0530

    📦NEW: add heading in IndWorkout screen

--- screens/IndWorkout.js
+++ screens/IndWorkout.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Text, StyleSheet } from 'react-native'
 
 import AddExercise from '../components/AddExercise'
 import Container from '../components/Container'
@@ -6,12 +7,14 @@ import DisplayExercises from '../components/DisplayExercises'
 
 import API_URL from '../constant'
 
+import convertDate from '../utils/convertDate'
+import convertDay from '../utils/convertDay'
 import fetchReq from '../utils/fetchReq'
 
 const IndWorkout = ({ route }) => {
   const [exercises, setExercises] = useState([])
   const { item } = route.params
-  const { _id } = item
+  const { _id, dayName, createdAt } = item
 
   const getExercises = async () => {
     const config = {
@@ -30,10 +33,22 @@ const IndWorkout = ({ route }) => {
 
   return (
     <Container>
+      <Text style={styles.heading}>
+        {dayName} ({convertDay(createdAt)} {convertDate(createdAt)})
+      </Text>
       <AddExercise id={_id} getExercises={getExercises} />
       <DisplayExercises exercises={exercises} />
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 22,
+    marginBottom: 10,
+    textTransform: 'capitalize',
+  },
+})
 
 export default IndWorkout
